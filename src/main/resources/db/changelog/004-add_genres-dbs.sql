9b743782b5f44897be6f1757de21368532bc9f0c
Isabelle Plante <iplantemn@gmail.com>
2020-08-24 18:19:42 -0500

    Populate `genre` table

---
+++ src/main/resources/db/changelog/004-add_genres-dbs.sql
@@ -0,0 +1,14 @@
+# Pre-populate the `genre` table with known movie genres
+
+INSERT INTO genre
+    (genre)
+VALUES
+    ('Action'),
+    ('Disaster'),
+    ('Superhero'),
+    ('Comedy'),
+    ('Romantic comedy'),
+    ('Drama'),
+    ('Indie'),
+    ('Animated'),
+    ('Science fiction');
