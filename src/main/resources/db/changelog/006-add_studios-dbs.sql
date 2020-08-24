d7fc6f3197981d3f7279c44bc7dd7bcb3f094eaf
Isabelle Plante <iplantemn@gmail.com>
2020-08-24 18:20:08 -0500

    Populate the `studio` table

---
+++ src/main/resources/db/changelog/006-add_studios-dbs.sql
@@ -0,0 +1,16 @@
+# Pre-populate the `roles` table with known movie studios
+
+INSERT INTO studio
+    (name)
+VALUES
+    ('Universal Pictures'),
+    ('Paramount Pictures'),
+    ('Warner Bros.'),
+    ('New Line Cinema'),
+    ('Walt Disney Pictures'),
+    ('20th Century Studios'),
+    ('Columbia Pictures'),
+    ('MGM Pictures'),
+    ('Marvel Studios'),
+    ('Pixar Animation Studios'),
+    ('Lucasfilm');
