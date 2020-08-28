ab8dccad4fa7733966e7a01d17b7daa32820a8bb
Isabelle Plante <iplantemn@gmail.com>
2020-08-27 21:06:29 -0500

    Fix find by cast endpoint

--- src/main/java/com/iplante/imdb/movies/repository/MovieRepository.java
+++ src/main/java/com/iplante/imdb/movies/repository/MovieRepository.java
@@ -1,8 +1,12 @@
 package com.iplante.imdb.movies.repository;
 
 import com.iplante.imdb.movies.entity.Movie;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 /**
  * Repository for {@link Movie} entities, additionally exposing CRUD operations as pageable and
@@ -14,4 +18,15 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
  */
 @RepositoryRestResource(collectionResourceRel = "movies", path = "movies")
 public interface MovieRepository extends PagingAndSortingRepository<Movie, Long> {
+
+    /**
+     * Find all movies for a given cast member.
+     *
+     * @param castId   the ID of the cast.
+     * @param pageable the pageable parameters.
+     * @return a list of movies attributed to the cast member.
+     */
+    @RestResource(path = "castId")
+    Page<Movie> findAllByCast(@Param("castId") long castId, Pageable pageable);
+
 }
