15956211588da58411871b75645f9e1078a3d7a0
Isabelle Plante <iplantemn@gmail.com>
2020-08-28 00:09:03 -0500

    Remove unused imports from `MovieController`

--- src/main/java/com/iplante/imdb/movies/controller/MovieController.java
+++ src/main/java/com/iplante/imdb/movies/controller/MovieController.java
@@ -8,8 +8,6 @@ import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Controller exposing custom CRUD operations on {@link Movie} that Spring Data REST cannot handle.
